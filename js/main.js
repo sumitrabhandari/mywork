d293f2327cbedda259cb1e5d60663fd642bbd9cc
sumitra <119399038+sumitrabhandari@users.noreply.github.com>
2023-03-13 14:35:18 +0545

    first project

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $(".four").owlCarousel({
+        items:4,
+        nav:true,
+        margin: 20,
+});
+  });
+$(document).ready(function(){
+    $(".three").owlCarousel({
+        items:3,
+        nav:true,
+        margin: 20,
+});
+  });
